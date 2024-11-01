2e31f78a1147da7209e9257c7f9c3e5f579bf1d1
Marque6299 <165474979+Marque6299@users.noreply.github.com>
2024-11-01 08:55:12 +0800

    Delete VBS Workshop directory

--- VBS Workshop/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for toggling the responsive menu
-document.getElementById('nav-toggle').addEventListener('click', function() {
-    var navLinks = document.getElementById('nav-links');
-    if (navLinks.classList.contains('show')) {
-        navLinks.classList.remove('show');
-    } else {
-        navLinks.classList.add('show');
-    }
-});
